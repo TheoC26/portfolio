67b4ee1e593d11ffdab01035e6ff264981929719
c25tc <c25tc@dalton.org>
2023-04-27 09:51:39 -0400

    adding form confirmation

--- public/form.js
+++ public/form.js
@@ -1,3 +1,4 @@
+const formRef = document.querySelector("form");
 const handleSubmit = (event) => {
   event.preventDefault();
 
@@ -9,8 +10,21 @@ const handleSubmit = (event) => {
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString(),
   })
-    .then(() => console.log("Form successfully submitted"))
+    .then(() => onSuccess(myForm))
     .catch((error) => alert(error));
 };
 
-document.querySelector("form").addEventListener("submit", handleSubmit);
+function onSuccess() {
+  document.getElementById("emailInput").value = "";
+  document.getElementById("messageInput").value = "";
+  document.getElementById("formButton").innerHTML = "Message sent!";
+  document.getElementById("formButton").style.backgroundColor =
+    "rgb(34, 197, 94)";
+    setTimeout(() => {
+      document.getElementById("formButton").innerHTML = "send";
+      document.getElementById("formButton").style.backgroundColor =
+        "black";
+    }, 2000);
+}
+
+formRef.addEventListener("submit", handleSubmit);
